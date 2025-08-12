dc8944e84962c43cc8fa3c02d21d10dc77e9695c
Hammad Afzal <hammadafzal05@gmail.com>
2025-08-12 13:40:16 +0100

    initialized the project

---
+++ agent.py
@@ -0,0 +1,10 @@
+from typing import Annotated, Sequence, TypedDict
+from dotenv import load_dotenv
+from langchain_core.messages import BaseMessage, ToolMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, END
+from langgraph.prebuilt import ToolNode
+
+load_dotenv()
