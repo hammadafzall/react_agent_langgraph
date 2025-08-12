6d95460c8babcfb0a1d7abd9f8c4662dd98c9109
Hammad Afzal <hammadafzal05@gmail.com>
2025-08-12 16:27:55 +0100

    Finished agent project

--- agent.py
+++ agent.py
@@ -8,3 +8,73 @@ from langgraph.graph import StateGraph, END
 from langgraph.prebuilt import ToolNode
 
 load_dotenv()
+
+class AgentState(TypedDict):
+    messages:Annotated[Sequence[BaseMessage], add_messages]
+
+@tool
+def add(a:int, b:int):
+    """This is addition function that adds two numbers"""
+    return a+b
+
+@tool
+def subtract(a:int, b:int):
+    """This is subtraaction function that subtracts two numbers"""
+    return a-b
+
+@tool
+def multiply(a:int, b:int):
+    """This is multiplication function that multiply two numbers"""
+    return a*b
+
+
+tools = [add, subtract, multiply]
+
+model = ChatOpenAI(model_name="gpt-4o").bind_tools(tools)
+
+def model_call(state:AgentState)-> AgentState:
+    system_prompt = SystemMessage(content='You are my AI assistant please answer my query to the best of your knowledge')
+    response = model.invoke([system_prompt] + state['messages'])
+    return {"messages":[response]}
+
+def should_continue(state:AgentState):
+    messages = state['messages']
+    last_message = messages[-1]
+    if not last_message.tool_calls:
+
+        return "end"
+    else:
+        return "continue"
+
+graph = StateGraph(AgentState)
+graph.add_node("our_agent", model_call)
+
+tool_node = ToolNode(tools=tools)
+graph.add_node("tools", tool_node)
+
+graph.set_entry_point("our_agent")
+
+graph.add_conditional_edges(
+    "our_agent",
+    should_continue,
+    {
+        "continue": "tools",
+        "end": END,
+    }
+)
+
+graph.add_edge("tools", "our_agent")
+
+app = graph.compile()
+
+def print_stream(stream):
+    for s in stream:
+        message= s["messages"][-1]
+        if isinstance(message,tuple):
+            print(message)
+        else:
+            message.pretty_print()
+
+inputs = {"messages": [("user", "Add 96 + 99 and multiply the result by 0.")]}
+
+print_stream(app.stream(inputs, stream_mode="values"))
